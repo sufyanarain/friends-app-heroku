09124f2541f52458b264f58ab8dad7cd0481e120
sufyanarain <sufyan822@gmail.com>
2022-01-05 20:45:55 +0500

    sd

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const app = express();
 const publicPath = path.join(__dirname, 'public', 'build');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
+
+
+app.get('/api/users',async (req,res)=>{
+   const user=[
+      {user:"saleemabdullah764@gmail.com", password:"123456"},
+      {user:"asad@gmail.com", password:"123456"},
+      {user:"anas@gmail.com", password:"123456"},
+      {user:"saif@gmail.com", password:"123456"}
+   ]
+   res.send(user)
+})
+app.get('/', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
 app.listen(port, () => {
    console.log('Server is up!');
 });
-
